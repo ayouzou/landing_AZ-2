ea5e16500caf9e5d167df01c71861c0c430e43f7
ayoub <ayoubzouiine@gmail.com>
2023-12-30 14:09:49 +0100

    added a new project to github

---
+++ src/constants/index.js
@@ -0,0 +1,197 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Rewards",
+    content:
+      "The best credit cards offer some tantalizing combinations of promotions and prizes",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Secured",
+    content:
+      "We take proactive steps make sure your information and transactions are secure.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Balance Transfer",
+    content:
+      "A balance transfer credit card can save you a lot of money in interest charges.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+    name: "AYOUB ZOUINE",
+    title: "Founder & Leader",
+    img: 'https://media.licdn.com/dms/image/D4E03AQEww_P1J7sI3A/profile-displayphoto-shrink_200_200/0/1698912369731?e=1709164800&v=beta&t=5R_47sk-ZWBqpSLqTihd_0Aa0usdtbUYIYyGMwZkASM',
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Money makes your life easier. If you're lucky to have it, you're lucky.",
+    name: "MOHAMMED BERMIME",
+    title: "softwore developer",
+    img: 'https://media.licdn.com/dms/image/D4E03AQGLo6bi3ZEhwA/profile-displayphoto-shrink_200_200/0/1694796325797?e=1709164800&v=beta&t=bypBRdcpX9BbjZyjMtMjNYIWAcmRp-5m6ZRG4Am8f4Q',
+  },
+  {
+    id: "feedback-3",
+    content:
+      "It is usually people in the money business, finance, and international trade that are really rich.",
+    name: "SOULAIMANE",
+    title: "softwore developer",
+    img: 'https://media.licdn.com/dms/image/D4E03AQG8cNf5WoomFQ/profile-displayphoto-shrink_200_200/0/1699383498400?e=1709164800&v=beta&t=9LQr1wA7G1RIWViQN6EpQp-1HvtTSk8xbipWuwMmRl0',
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "User Active",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Company",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Transaction",
+    value: "$230M+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.AZ.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.AZ.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.AZ.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.AZ.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.AZ.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.AZ.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.AZ.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.AZ.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.AZ.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.AZ.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.AZ.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.AZ.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
