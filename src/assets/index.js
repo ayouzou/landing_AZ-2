9d2d70b6f3239d8265ccb7bf68341819e5e59461
ayoub <ayoubzouiine@gmail.com>
2023-12-30 14:17:52 +0100

    added a new project to github

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,9 +4,9 @@ import binance from "./binance.png";
 import card from "./card.png";
 import coinbase from "./coinbase.png";
 import dropbox from "./dropbox.png";
-import logo from "./logo.svg";
+
 import quotes from "./quotes.svg";
-import robot from "./robot.png";
+
 import send from "./Send.svg";
 import shield from "./Shield.svg";
 import star from "./Star.svg";
@@ -20,9 +20,7 @@ import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
 import twitter from "./twitter.svg";
-import people01 from "./people01.png";
-import people02 from "./people02.png";
-import people03 from "./people03.png";
+
 
 export {
   airbnb,
@@ -31,9 +29,7 @@ export {
   card,
   coinbase,
   dropbox,
-  logo,
   quotes,
-  robot,
   send,
   shield,
   star,
@@ -47,7 +43,5 @@ export {
   instagram,
   linkedin,
   twitter,
-  people01,
-  people02,
-  people03,
+ 
 };
